62623191f19feb6f5f8cd2e80510cfe49c35c96e
hilelley <hilelley@gmail.com>
2020-06-29 14:36:09 +0300

    מחיקת דברים מיותרים

--- apps/mywebsite/js/1prevent_default.js
+++
@@ -1,8 +0,0 @@
-/******************************************
- * EXAMPLE 1
- * prevent default
- *****************************************/
-function validateForm(e) {
-  console.log(e);
-  e.preventDefault();
-}
